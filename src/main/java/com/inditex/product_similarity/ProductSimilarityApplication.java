45b63fdae1bc74349fb62c003848ca3f3edac4e7
gerardgsl <gerardsoley1998@gmail.com>
2025-07-10 18:15:01 +0200

    initial project setup with Spring Boot

---
+++ src/main/java/com/inditex/product_similarity/ProductSimilarityApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.product_similarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSimilarityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSimilarityApplication.class, args);
+	}
+
+}
